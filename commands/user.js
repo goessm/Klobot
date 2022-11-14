4903a9928b931954efc61261f682896fbf90b2af
goessm <44338544+goessm@users.noreply.github.com>
2022-11-14 13:19:23 +0100

    fix: command names must be all lowercase

--- commands/user.js
+++ commands/user.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('joinAge')
+        .setName('joinage')
         .setDescription('Shows when you joined the server.'),
     async execute(interaction) {
         await interaction.reply(`${interaction.user.username} joined on ${interaction.member.joinedAt}.`);
